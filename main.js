f8c5bbc66a7880f1d52d8650b553e39c9fa73d9d
xmetrix <xmetrix@gmail.com>
2015-11-04 14:01:32 -0800

    added resize

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
 $(document).ready(function(){
-	$('#sections div').each(function(i,v){
-		$(v).css('width', $(window).width()  + 'px');});
-	function clearActiveLink(){	$('#links').find('li').each(function(i,v){
+
+	 resizeDivs();
+	
+	$(window).resize(function(){
+ 		resizeDivs();
+	});
+			
+	function clearActiveLink(){
+		$('#links').find('li').each(function(i,v){
 			$(v).removeClass('active');
-		});	}
-	function setActive(whichElement){if(!$(whichElement).hasClass('active')){
+		});
+	}
+	function setActive(whichElement){
+		if(!$(whichElement).hasClass('active')){
 			clearActiveLink();
 			$(whichElement).addClass('active');	
-		}	}
+		}
+	}
+	function resizeDivs(){
+		$('#sections div').each(function(i,v){
+			$(v).css('width', $(window).width()  + 'px'); //sets witdth of section divs to 100% window width. 
+		});
+	}
+
 	$('#links li').click(function(){
-		var w = -($(this).attr('data-index')*  $(window).width());
-		console.log('w: ' + w)
-		$('#sections').animate({
-		 	marginLeft:  w + 'px'
-		},300);
+		var w = -($(this).attr('data-index') * $(window).width());
+		$('#sections').animate({marginLeft:  w + 'px'},300);
 		setActive($(this));
 	});
 
